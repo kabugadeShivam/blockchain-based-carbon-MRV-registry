b5f62fb97129edba1fd0dbd18d99467bd9175315
VisionCrafter <kabugadeshivam248@gmail.com>
2026-09-13 23:13:15 +0530

    fix: compile OpenZeppelin with Cancun EVM target

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,10 @@
 import "@nomicfoundation/hardhat-toolbox";
 
 export default {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: { evmVersion: "cancun" }
+  },
   networks: {
     hardhat: {},
     localhost: { type: "http", url: "http://127.0.0.1:8545" }
