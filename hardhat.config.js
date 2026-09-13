e452071dc0dde203f8837c761655760f6569606c
VisionCrafter <kabugadeshivam248@gmail.com>
2026-09-13 23:07:54 +0530

    fix: align Hardhat compiler with OpenZeppelin dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,9 @@
 import "@nomicfoundation/hardhat-toolbox";
 
 export default {
-  solidity: "0.8.24",
+  solidity: "0.8.27",
   networks: {
     hardhat: {},
-    localhost: {
-      type: "http",
-      url: "http://127.0.0.1:8545"
-    }
+    localhost: { type: "http", url: "http://127.0.0.1:8545" }
   }
 };
